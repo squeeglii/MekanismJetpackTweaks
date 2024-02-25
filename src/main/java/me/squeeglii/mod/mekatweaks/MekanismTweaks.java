a2aee5e78ae1077da802d3d8dfac2ac26b187849
Will <16578982+CloudG360@users.noreply.github.com>
2024-02-25 01:56:38 +0000

    Base forge mod setup w/ mekanism

---
+++ src/main/java/me/squeeglii/mod/mekatweaks/MekanismTweaks.java
@@ -0,0 +1,37 @@
+package me.squeeglii.mod.mekatweaks;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod("mekatweaks")
+public class MekanismTweaks {
+
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MekanismTweaks() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+
+    }
+}
